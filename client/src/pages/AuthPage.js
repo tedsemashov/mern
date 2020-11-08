8431f0b2fdeda466f746dc7569830ab6cef4f466
Ted Semashov <tsemashov@amplify.com>
2020-11-08 19:22:56 +0200

    finished with Authorization

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,91 @@
-import React from "react";
+import React, { useContext, useEffect, useState} from "react";
+import { useHttp } from "../hooks/http.hook";
+import { useMessage } from "../hooks/message.hook";
+import { AuthContext } from "../context/AuthContext";
 
-export const AuthPage =() => {
+export const AuthPage = () => {
+    const auth = useContext(AuthContext)
+    const message = useMessage();
+    const { loading, error, request, clearError } = useHttp();
+    const [form, setForm] = useState({
+        email: '', password: ''
+    });
+
+    useEffect(() => {
+        message(error);
+        clearError();
+    }, [error, message, clearError]);
+
+    useEffect(() => {
+        window.M.updateTextFields();
+    }, []);
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value })
+    };
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form});
+            message(data.message);
+        } catch (e) {}
+    };
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form});
+            auth.login(data.token, data.userId);
+        } catch (e) {}
+    };
+
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>MERN</h1>
+                <div className="card blue-grey darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Authorization</span>
+                        <div>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Insert email"
+                                    id="email"
+                                    type="text"
+                                    name="email"
+                                    onChange={changeHandler}
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Insert password"
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    onChange={changeHandler}
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button
+                            className="btn yellow darken-4"
+                            disabled={loading}
+                            onClick={loginHandler}
+                        >
+                            Log in
+                        </button>
+                        <button
+                            className="btn gray lighten-1 black-text"
+                            onClick={registerHandler}
+                            disabled={loading}
+                        >
+                            Register
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { LinksPage } from "./pages/LinksPage";
 import { CreatePage } from "./pages/CreatePage";
 import { DetailPage } from "./pages/DetailPage";
 import { AuthPage } from "./pages/AuthPage";
@@ -10,13 +9,10 @@ export const useRoutes = isAuthenticated => {
     if(isAuthenticated) {
         return (
             <Switch>
-                <Route path="/links" exact>
-                    <LinksPage />
-                </Route>
                 <Route path="/create" exact>
                     <CreatePage />
                 </Route>
-                <Route path="/detail/:id" >
+                <Route path="/detail" >
                     <DetailPage />
                 </Route>
                 <Redirect to="/create" />
